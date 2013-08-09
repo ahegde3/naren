cdc4674244e89e1127bb795d95617262a4a25c99
ns.bisht <nsbisht@ubuntu-sta113127.(none)>
2013-08-09 18:45:28 -0400

    hacker Rank

---
+++ JavaStandalone/src/com/narren/hackerrank/JourneyToMoon.java
@@ -0,0 +1,20 @@
+package com.narren.hackerrank;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+public class JourneyToMoon {
+
+    private void perfomeComputation() {
+        List<Set<Integer>> setList = new ArrayList<Set<Integer>>();
+        for (int i = 0; i < setList.size(); i++) {
+            Set<Integer> set = setList.get(i);
+            for (int j = i+1; j < setList.size(); j++) {
+                if () {
+                    
+                }
+            }
+        }
+    }
+}
