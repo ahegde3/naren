fc9a036aeb45fae40b24bca443de0f1b9de8e43f
Naren <ns.bisht@sta.samsung.com>
2015-02-18 21:48:09 -0500

    Sherlock code

---
+++ JavaStandalone_Hacker_rank/src/com/narren/hackerRank/SherlockandTheBeast.java
@@ -0,0 +1,54 @@
+package com.narren.hackerRank;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+
+public class SherlockandTheBeast {
+	static BufferedReader in = new BufferedReader(new InputStreamReader(
+			System.in));
+	static StringBuilder out = new StringBuilder();
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		for (int i = 0; i < T; i++) {
+			int N = sc.nextInt();
+			if (N < 3) {
+				System.out.println("-1");
+				continue;
+			}
+			int fives = N/3;
+			int threes = N%3;
+			if (threes == 0) {
+				// All fives
+				//System.out.println(fives);
+				StringBuilder str = new StringBuilder();
+				for (int j = 0; j < fives*3; j++) {
+					str.append("5");
+				}
+				System.out.println(str);
+				continue;
+			}
+			int R = 5 - threes;
+			int L = 3*fives - R;
+			if (L%3 == 0) {
+				int finalF = L;
+				int finalT = N - L;
+				//System.out.println(finalF + " " + finalT);
+				StringBuilder str = new StringBuilder();
+				for (int j = 0; j < finalF; j++) {
+					str.append("5");
+				}
+				for (int j = 0; j < finalT; j++) {
+					str.append("3");
+				}
+				System.out.println(str);
+			} else {
+				System.out.println("-1");
+			}
+			
+			
+		}
+	}
+}
