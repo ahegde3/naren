2abb57c47bfe75366dea56ea25329b7b3f64f9f3
Naren <ns.bisht@sta.samsung.com>
2015-07-26 16:20:37 -0400

    Completed Dijsktra algo

--- JavaStandalone_Hacker_rank/src/com/narren/graph/BinaryHeap.java
+++ JavaStandalone_Hacker_rank/src/com/narren/graph/BinaryHeap.java
@@ -1,25 +1,61 @@
 package com.narren.graph;
 
+import java.util.Scanner;
+
 class PriorityElement {
 	int weight;
 	
 }
+/**
+ * 
+ * @author ns.bisht
+ *
+ */
 public class BinaryHeap {
 
-	PriorityElement[] pQueue = new PriorityElement[10];
-	int qHead = 1;
+	static PriorityElement[] pQueue = null;
+	static int qHead = 0;
 	public static void main(String[] args) {
-		
+		/**
+		 13
+10
+8
+7
+5
+6
+15
+25
+20
+11
+9
+17
+17
+9
+		 */
+		Scanner s = new Scanner(System.in);
+		int count = s.nextInt();
+		pQueue = new PriorityElement[count + 1];
+		for (int i = 1; i <= count; i++) {
+			PriorityElement e = new PriorityElement();
+			e.weight = s.nextInt();
+			//insert(e);
+			pQueue[i] = e;
+			qHead++;
+		}
+		heapify();
+		for (int i = 1; i <= count; i++) {
+			System.out.println(pQueue[i].weight);
+		}
 	}
 	
 	/**
 	 * Insert into the priority queue
 	 * @param element
 	 */
-	void insert (PriorityElement element) {
+	static void insert (PriorityElement element) {
+		qHead++;
 		pQueue[qHead] = element;
 		int tempHead = qHead;
-		qHead++;
 		while (tempHead/2 > 0 && pQueue[tempHead/2].weight > element.weight) {
 			swap(tempHead, tempHead/2);
 			tempHead = tempHead/2;
@@ -52,11 +88,30 @@ public class BinaryHeap {
 			qHead--;
 		}
 	}
-	void swap(int i, int j) {
+	static void swap(int i, int j) {
 		PriorityElement temp = pQueue[i];
 		pQueue[i] = pQueue[j];
 		pQueue[j] = temp;
 	}
 	
-	
+	static void heapify () {
+		//Get the first parent from last.
+		int parentIndex = qHead/2;
+		
+		while (parentIndex > 0) {
+			int tempParentIndex = parentIndex;
+			while (tempParentIndex <= qHead/2 &&
+					(pQueue[tempParentIndex].weight > pQueue[tempParentIndex * 2].weight ||
+					pQueue[tempParentIndex].weight > pQueue[tempParentIndex * 2 + 1].weight)) {
+				if (pQueue[tempParentIndex * 2].weight > pQueue[tempParentIndex * 2 + 1].weight) {
+					swap(tempParentIndex * 2 + 1, tempParentIndex); //swap parent and right child
+					tempParentIndex = tempParentIndex * 2 + 1;
+				} else {
+					swap(tempParentIndex*2, tempParentIndex); //swap parent and left child
+					tempParentIndex = tempParentIndex * 2;
+				}
+			}
+			parentIndex--;
+		}
+	}
 }
--- JavaStandalone_Hacker_rank/src/com/narren/graph/DijsktraAlgo.java
+++ JavaStandalone_Hacker_rank/src/com/narren/graph/DijsktraAlgo.java
@@ -2,9 +2,29 @@ package com.narren.graph;
 
 import java.util.Scanner;
 
+/**
+ * 
+ * @author ns.bisht
+ *
+ */
 public class DijsktraAlgo {
 
+	static int[] shortestArray;
 	public static void main(String[] args) {
+		/**
+		 * 
+6
+9
+1 2 2
+1 4 5
+1 3 1
+2 6 1
+2 4 2
+3 4 1
+4 6 3
+4 5 2
+5 3 4
+		 */
 		int vertices, edges, i, v, v1, v2;
 		Scanner s = new Scanner(System.in);
 
@@ -23,53 +43,57 @@ public class DijsktraAlgo {
 		for (i = 1; i <= edges; ++i) {
 			v1 = s.nextInt();
 			v2 = s.nextInt();
-			adjanceyList[v1] = insert(adjanceyList[v1], v2);
+			int weight = s.nextInt();
+			adjanceyList[v1] = insert(adjanceyList[v1], v2, weight);
 		}
 
-		//Printing Adjacency List
-		System.out.println("\nAdjacency List -\n");
-		for (i = 1; i <= vertices; ++i) {
-			System.out.println(i + "-> ");
-
-			Node temp = adjanceyList[i];
-
-			while (temp != null) {
-				System.out.println(temp.value + " -> ");
-				temp = temp.next;
-			}
-
-			System.out.println("NULL\n");
+		shortestArray = new int[adjanceyList.length];
+		dijsktra(adjanceyList);
+		for (i = 1; i < shortestArray.length; i++) {
+			System.out.println(i + "-> " + shortestArray[i]);
 		}
 
 	}
 	static void dijsktra (Node[] adjanceyList) {
-		int[] shortestArray = new int[adjanceyList.length];
 		BinaryHeapHelper binaryHeap = new DijsktraAlgo().new BinaryHeapHelper();
 		binaryHeap.pQueue = new PriorityElement[adjanceyList.length];
-		for (int i = 0; i < adjanceyList.length; i++) {
-			shortestArray[i] = Integer.MAX_VALUE;
-		}
+//		for (int i = 0; i < adjanceyList.length; i++) {
+//			shortestArray[i] = Integer.MAX_VALUE;
+//		}
 		
 		shortestArray[1] = 0;
 		PriorityElement pe = new DijsktraAlgo().new PriorityElement();
 		pe.weight = 0;
 		pe.index = 1;
 		binaryHeap.insert(pe);
+		int weight = 0;
 		
 		while (!binaryHeap.isQueueEmpty()) {
 			PriorityElement min = binaryHeap.extractMin();
 			Node temp = adjanceyList[min.index];
+			//shortestArray[min.index] += min.weight;
 			if (temp != null) {
 
-				PriorityElement elem = new DijsktraAlgo().new PriorityElement();
-				elem.weight = temp.value;
-				elem.index = min.index;
-				binaryHeap.insert(elem);
-				while (temp.next != null) {
+				while (temp != null) {
+					int tempPe = binaryHeap.getIndex(temp.value);
+					if (tempPe != -1 && (binaryHeap.pQueue[tempPe].weight > weight + temp.weight)) {
+						binaryHeap.delete(tempPe);
+						binaryHeap.heapify();
+						
+					} else if (tempPe != -1) {
+						temp = temp.next;
+						continue;
+					}
+					if(tempPe == -1 && shortestArray[temp.value] > 0
+							&& shortestArray[temp.value] < temp.weight) {
+						temp = temp.next;
+						continue;
+					}
 					PriorityElement ele = new DijsktraAlgo().new PriorityElement();
-					ele.weight = temp.next.value;
-					ele.index = min.index;
+					ele.weight = shortestArray[min.index] + temp.weight;
+					ele.index = temp.value;
 					binaryHeap.insert(ele);
+					shortestArray[temp.value] = ele.weight;
 					temp = temp.next;
 				}				
 			}
@@ -82,6 +106,7 @@ public class DijsktraAlgo {
 		int value;
 		Node next;
 		boolean visited;
+		int weight;
 	}
 	/**
 	 * This is exactly as Head Insertion of a Linked List.
@@ -91,11 +116,12 @@ public class DijsktraAlgo {
 	 * @param value
 	 * @return
 	 */
-	private static Node insert (Node head, int value) {
+	private static Node insert (Node head, int value, int weight) {
 		Node p = new DijsktraAlgo().new Node();
 		p.value = value;
 		p.next = head;
 		p.visited = false;
+		p.weight = weight;
 		return p;
 	}
 	class PriorityElement {
@@ -110,6 +136,17 @@ public class DijsktraAlgo {
 
 		boolean isQueueEmpty() {
 			return qHead == 0;
+		}
+		
+		int getIndex(int index) {
+			for (int i = 1; i <= pQueue.length; i++) {
+				if (pQueue[i] == null) {
+					return -1;
+				} else if (pQueue[i].index == index) {
+					return i;
+				}
+			}
+			return -1;
 		}
 		/**
 		 * Insert into the priority queue
@@ -130,6 +167,10 @@ public class DijsktraAlgo {
 				swap(index, qHead);
 				pQueue[qHead] = null;
 				qHead--;
+				if(index/2 < 1) {
+					// deleting first element
+					return;
+				}
 				if (pQueue[index].weight > pQueue[index/2].weight) {
 					// percolating down
 					int temp = index;
@@ -177,6 +218,7 @@ public class DijsktraAlgo {
 
 			while (parentIndex > 0) {
 				int tempParentIndex = parentIndex;
+				if (pQueue[tempParentIndex*2 + 1] != null) {
 				while (tempParentIndex <= qHead/2 &&
 						(pQueue[tempParentIndex].weight > pQueue[tempParentIndex * 2].weight ||
 								pQueue[tempParentIndex].weight > pQueue[tempParentIndex * 2 + 1].weight)) {
@@ -188,6 +230,7 @@ public class DijsktraAlgo {
 						tempParentIndex = tempParentIndex * 2;
 					}
 				}
+			}
 				parentIndex--;
 			}
 		}
