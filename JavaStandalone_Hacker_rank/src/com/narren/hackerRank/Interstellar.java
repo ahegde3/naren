ba0a5f9d223fa61280f6005a109ea5371a6d15ff
NarenHomeLinux <narendrasingh.bisht@gmail.com>
2016-03-02 22:18:57 -0500

    Solving the ineterstellar

---
+++ JavaStandalone_Hacker_rank/src/com/narren/hackerRank/Interstellar.java
@@ -0,0 +1,21 @@
+package com.narren.hackerRank;
+
+import java.util.Scanner;
+
+public class Interstellar {
+
+	static int[][] arr;
+	static int D;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int T = sc.nextInt();
+		while(T > 0) {
+			int N = sc.nextInt();
+			D = 2 + N*2 + 1;
+			arr = new int[D][D];
+			while
+			
+			T--;
+		}
+	}
+}
