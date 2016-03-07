9523efeed8a4f6c76a99028533443a1e630a8552
NarenHomeLinux <narendrasingh.bisht@gmail.com>
2016-03-06 22:45:17 -0500

    Added Cookies problem

---
+++ JavaStandalone_Hacker_rank/src/com/narren/hackerRank/Cookies.java
@@ -0,0 +1,36 @@
+package com.narren.hackerRank;
+
+import java.util.Scanner;
+
+public class Cookies {
+
+	static int[]arr;
+	static int count;
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		arr = new int[N];
+		for(int i = 0; i < N; i++) {
+			arr[i] = sc.nextInt();
+		}
+		process(0);
+	}
+
+	static int process(int i) {
+		if (i >= arr.length) {
+			return 0;
+		}
+		if(i < (arr.length - 1)) {
+			if (arr[i] < arr[i + 1]) {
+				count += 1;	
+			} else if (arr[i] > arr[i + 1]) {
+				count += process(i + 1);	
+			} else {
+				count += process(i + 1);
+			} 
+		} else {
+			return 1;
+		}
+		return 1;
+	}
+}
