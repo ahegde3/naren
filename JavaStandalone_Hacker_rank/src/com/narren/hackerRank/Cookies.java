a14031d6c05c1111a413f1e2d061e162d680e198
Narendra Bisht <ns.bisht@sea.samsung.com>
2016-03-08 18:52:25 -0500

    Solved the Candies problem

--- JavaStandalone_Hacker_rank/src/com/narren/hackerRank/Cookies.java
+++ JavaStandalone_Hacker_rank/src/com/narren/hackerRank/Cookies.java
@@ -6,33 +6,39 @@ public class Cookies {
 
 	static int[]arr;
 	static int count;
-	static boolean[] visited;
+	static int[] visited;
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		arr = new int[N];
-		visited = new boolean[N];
+		visited = new int[N];
 		for(int i = 0; i < N; i++) {
 			arr[i] = sc.nextInt();
 		}
-		process(0);
+		process();
+		int sum = 0;
+		for(int i = 0; i < visited.length; i++) {
+			sum += visited[i];
+		}
+		System.out.println(sum);
 	}
 
-	static int process(int i) {
-		if (i >= arr.length) {
-			return 0;
+	static void process() {
+		//first iterate forward
+		for(int i = 0; i < arr.length; i++) {
+			visited[i] = 1;	
+			if (i > 0) {
+				if (arr[i] > arr[i - 1]) {
+					visited[i] = (1 + visited[i - 1]);
+				}
+			}
 		}
-		if(i < (arr.length - 1)) {
-			if (arr[i] < arr[i + 1]) {
-				count += 1;	
-			} else if (arr[i] > arr[i + 1]) {
-				count += process(i + 1);	
-			} else {
-				count += process(i + 1);
-			} 
-		} else {
-			return 1;
+
+		// Now backwards
+		for(int i = arr.length - 2; i >= 0; i--) {
+			if (arr[i] > arr[i + 1]) {
+				visited[i] = Math.max(visited[i], visited[i + 1] + 1);
+			}
 		}
-		return 1;
 	}
 }
