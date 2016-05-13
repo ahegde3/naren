f2133b3587b08413b6445bd623546b7bd07d319c
newPcHome <narendrasingh.bisht@gmail.com>
2016-05-13 19:09:43 -0400

    Added finding all permutations of a given string

---
+++ JavaStandalone_Hacker_rank/src/com/narren/coding/interview/PermutationOfString.java
@@ -0,0 +1,28 @@
+package com.narren.coding.interview;
+
+public class PermutationOfString {
+
+	
+	static void print(char[] c, int l, int r) {
+		if(l == r) {
+			System.out.println(c);
+		} else {
+		for(int i = l; i <= r; i++) {
+			swap(c, l, i);
+			print(c, l + 1, r);
+			swap(c, l, i);
+		}
+		}
+	}
+	
+	private static void swap(char[] c, int i, int j) {
+		char temp = c[i];
+		c[i] = c[j];
+		c[j] = temp;
+	}
+	
+	public static void main(String[] args) {
+		String s = "abcd";
+		print(s.toCharArray(), 0, 3);
+	}
+}
