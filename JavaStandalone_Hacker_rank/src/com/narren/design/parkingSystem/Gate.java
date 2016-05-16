0d8401b2e159cb7d356b9a4f6a5cd8714f0d6457
Naren <ns.bisht@sta.samsung.com>
2016-05-16 17:22:59 -0400

    First cut Parking system design

---
+++ JavaStandalone_Hacker_rank/src/com/narren/design/parkingSystem/Gate.java
@@ -0,0 +1,13 @@
+package com.narren.design.parkingSystem;
+
+/**
+ * Class that corresponds to the entry and exit gates
+ * @author nsbisht
+ *
+ */
+public class Gate {
+
+	protected void generateToken(){
+		
+	}
+}
