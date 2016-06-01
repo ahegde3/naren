0ac4918c68ee0f6291296c3afb4b9878dd34414d
Naren <ns.bisht@sta.samsung.com>
2016-06-01 18:15:31 -0400

    Added finding permutation of a string

---
+++ JavaStandalone_Hacker_rank/src/com/narren/coding/interview/PermutationOfStringSelf.java
@@ -0,0 +1,42 @@
+package com.narren.coding.interview;
+
+public class PermutationOfStringSelf {
+
+	char[] result;
+	void process(String s, int[] count, int level) {
+		for(int i= 0; i < count.length; i++){
+			if(count[i] > 0) {
+				result[level] = (char) i;
+				count[i] = count[i] - 1;
+				process(s, count, level + 1);
+				count[i] = count[i] + 1;
+			}
+		}
+		for(int i = 0; i < 123; i++) {
+			if(count[i] != 0) {
+				break;
+			}
+			if(i == 122) {
+				for (char c : result) {
+					System.out.print(c);	
+				}	
+				System.out.println();
+			}
+		}
+
+		
+
+	}
+
+	public static void main(String[] args) {
+		PermutationOfStringSelf self = new PermutationOfStringSelf();
+		
+		String s = "aacd";
+		self.result = new char[s.length()];
+		int[] count = new int[123];
+		for(int i= 0; i < s.length(); i++){
+			count[s.charAt(i)]++;
+		}
+		self.process(s, count, 0);
+	}
+}
