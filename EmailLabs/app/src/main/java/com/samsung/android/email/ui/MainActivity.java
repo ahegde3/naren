7b476a04602bb5da7a1372ed13aade574f4519d0
Naren <ns.bisht@sta.samsung.com>
2016-06-02 18:04:12 -0400

    Added the main activity

--- EmailLabs/app/src/main/java/com/samsung/android/email/ui/MainActivity.java
+++ EmailLabs/app/src/main/java/com/samsung/android/email/ui/MainActivity.java
@@ -1,7 +1,34 @@
 package com.samsung.android.email.ui;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import com.samsung.android.email.R;
+import com.samsung.android.email.model.SampleData;
+
 /**
  * Created by nsbisht on 5/31/16.
  */
-public class MainActivity {
+public class MainActivity extends Activity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_recycler_list);
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.main_recycler_view);
+        mRecyclerView.setHasFixedSize(true);
+
+        mLayoutManager = new LinearLayoutManager(this);
+
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        mAdapter = new EmailListAdapter(SampleData.getSampleData());
+        mRecyclerView.setAdapter(mAdapter);
+    }
 }
