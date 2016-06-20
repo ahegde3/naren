97a6b3eb362bce7bc7a0de78fe3053560c078dbd
Naren <ns.bisht@sta.samsung.com>
2016-06-20 14:36:27 -0400

    added few methods

--- JavaStandalone_Hacker_rank/src/sotong/Locust.java
+++ JavaStandalone_Hacker_rank/src/sotong/Locust.java
@@ -4,6 +4,16 @@ import java.util.Scanner;
 
 public class Locust {
 
+	/**
+	 * 
+5
+3 0 2 7 10 13 17
+2 1 5 9 11
+2 7 12 14 18
+1 3 7
+2 4 6 15 16
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
@@ -14,6 +24,9 @@ public class Locust {
 			for(int j = 1; j <=intervals; j++) {
 				int start = sc.nextInt();
 				int end = sc.nextInt();
+				for(int k = start ; k <= end ; k++) {
+					arr[j - 1][k] = 1;
+				}
 				maxRight = Math.max(end, maxRight);
 			}
 		}
@@ -21,6 +34,31 @@ public class Locust {
 	}
 	
 	static void process(int[][] arr, int maxRight) {
-		
+		for(int row = arr.length - 1; row >= 0; row-- )
+		for(int col = 0; col < maxRight; col++) {
+			int count = getJumpCount(arr, col, row, 0);
+			System.out.println(count);
+		}
+	}
+	
+	static int nextStep(int[][] arr, int row, int col) {
+		if(row == 0) {
+			return -1;
+		}
+		while(row > 0) {
+			if(arr[row][col] == 1) {
+				return row;
+			}
+			row--;
+		}
+		return -1;
+	}
+	
+	static int getJumpCount(int arr[][], int curCol, int curRow, int jump) {
+		if(curRow == -1) {
+			return jump;
+		}
+		int jumpCount = getJumpCount(arr, curCol, nextStep(arr, curRow, curCol), jump + 1);
+		return jumpCount;
 	}
 }
