1decf6a384195cbae243947673736c19672bf70c
newPcHome <narendrasingh.bisht@gmail.com>
2016-07-28 08:26:36 -0400

    Updated the even order neighbors

--- JavaStandalone_Hacker_rank/src/sotong/ConstructionOfBaseStation.java
+++ JavaStandalone_Hacker_rank/src/sotong/ConstructionOfBaseStation.java
@@ -13,6 +13,35 @@ public class ConstructionOfBaseStation {
 
 	}
 
+	public static void getNeighbors(int[][] arr, int w, int h, int wRow, int hCol) {
+		Cell[] neighbors = new Cell[6];
+		int i = 0;
+		// We have two patterns for finding the neighbors, based on whether the row is even or odd
+		if((wRow & 1) == 0) {
+			//Even
+			if(wRow - 1 >= 0) {
+				neighbors[i++] = new Cell(wRow - 1, hCol);
+			}
+			if(wRow + 1 < w) {
+				neighbors[i++] = new Cell(wRow + 1, hCol);
+			}
+			if(hCol - 1 >= 0) {
+				neighbors[i++] = new Cell(wRow, hCol - 1);
+			}
+			if(hCol + 1 < h) {
+				neighbors[i++] = new Cell(wRow, hCol + 1);
+			}
+			if(wRow - 1 >= 0 && hCol - 1 >= 0) {
+				neighbors[i++] = new Cell(wRow - 1, hCol - 1);
+			}
+			if(wRow + 1 < w && hCol - 1 >= 0) {
+				neighbors[i++] = new Cell(wRow + 1, hCol - 1);
+			}
+		}
+
+	}
+
+
 	static class Cell {
 		int r;
 		int c;
@@ -36,7 +65,7 @@ public class ConstructionOfBaseStation {
 			head++;
 			stack[head] = cell;
 		}
-		
+
 		static Cell pop() {
 			Cell cell = null;
 			if(isEmpty()) {
