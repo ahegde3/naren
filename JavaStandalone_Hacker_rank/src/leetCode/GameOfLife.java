1f002fa4bd8fa58d24a05b576a783759aa3313c5
newPcHome <narendrasingh.bisht@gmail.com>
2016-08-05 08:42:19 -0400

    Added my version of Game of life

--- JavaStandalone_Hacker_rank/src/leetCode/GameOfLife.java
+++ JavaStandalone_Hacker_rank/src/leetCode/GameOfLife.java
@@ -1,74 +1,63 @@
 package leetCode;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-
 public class GameOfLife {
 
-	private Set<Coord> gameOfLife(Set<Coord> live) {
-		Map<Coord,Integer> neighbours = new HashMap<>();
-		for (Coord cell : live) {
-			for (int i = cell.i-1; i<cell.i+2; i++) {
-				for (int j = cell.j-1; j<cell.j+2; j++) {
-					if (i==cell.i && j==cell.j) continue;
-					Coord c = new Coord(i,j);
-					if (neighbours.containsKey(c)) {
-						neighbours.put(c, neighbours.get(c) + 1);
-					} else {
-						neighbours.put(c, 1);
+	int[] movesX = new int[]{0, 0, -1, 1, -1, 1, 1, -1};
+	int[] movesY = new int[]{1, -1, 0, 0, 1, 1, -1, -1};
+
+	int[] getNeighbors(int[][] board, int r, int c, int R, int C) {
+		int[] neighbors = new int[]{-1, -1, -1, -1, -1, -1, -1, -1};
+		int index = 0;
+		for(int i = 0; i < 8; i++) {
+			int xVal = r + movesX[i];
+			int yVal = c + movesY[i];
+			if(xVal < 0 || yVal < 0 || xVal >= R || yVal >= C) {
+				continue;
+			}
+			neighbors[index++] = board[xVal][yVal];
+		}
+		return neighbors;
+
+	}
+
+	public void gameOfLife(int[][] board) {
+		int R = board.length;
+		int C = board[0].length;
+		for(int i = 0; i < R; i++) {
+			for(int j = 0; j < C; j++) {
+				int[] neighbors = getNeighbors(board, i, j, R, C);
+				int k = 0;
+				int ones = 0;
+				while(k < 8 && neighbors[k] != -1) {
+					if(neighbors[k] == 1) {
+						ones++;
+					}
+					k++;
+				}
+				if(ones < 2 || ones > 3) {
+					if((board[i][j] & 1) == 1) {
+						board[i][j] = 3;
+					}
+					
+				} else if(ones == 3) {
+					if((board[i][j] & 1) == 0) {
+						board[i][j] = 2;
 					}
 				}
 			}
 		}
-		Set<Coord> newLive = new HashSet<>();
-		for (Map.Entry<Coord,Integer> cell : neighbours.entrySet())  {
-			if (cell.getValue() == 3 || cell.getValue() == 2 && live.contains(cell.getKey())) {
-				newLive.add(cell.getKey());
-			}
-		}
-		return newLive;
-	}
-
-	private static class Coord {
-		int i;
-		int j;
-		private Coord(int i, int j) {
-			this.i = i;
-			this.j = j;
-		}
-		public boolean equals(Object o) {
-			return o instanceof Coord && ((Coord)o).i == i && ((Coord)o).j == j;
-		}
-		public int hashCode() {
-			int hashCode = 1;
-			hashCode = 31 * hashCode + i;
-			hashCode = 31 * hashCode + j;
-			return hashCode;
-		}
-	}
-
-	public void gameOfLife(int[][] board) {
-		Set<Coord> live = new HashSet<>();
-		int m = board.length;
-		int n = board[0].length;
-		for (int i = 0; i<m; i++) {
-			for (int j = 0; j<n; j++) {
-				if (board[i][j] == 1) {
-					live.add(new Coord(i,j));
+		
+		for(int i = 0; i < R; i++) 
+			for(int j = 0; j < C; j++) {
+				if(board[i][j] == 3) {
+					board[i][j] = 0;
+				}
+				if(board[i][j] == 2) {
+					board[i][j] = 1;
 				}
 			}
-		};
-		live = gameOfLife(live);
-		for (int i = 0; i<m; i++) {
-			for (int j = 0; j<n; j++) {
-				board[i][j] = live.contains(new Coord(i,j))?1:0;
-			}
-		};
-
 	}
-	
+
 	public static void main(String[] args) {
 		int[][] board = new int[][]{
 			{0, 0, 0, 0, 1, 1},
@@ -78,7 +67,7 @@ public class GameOfLife {
 			{1, 0, 0, 1, 0, 1}
 		};
 		new GameOfLife().gameOfLife(board);
-		
+
 		for(int i = 0; i < 5; i++) {
 			for(int j = 0; j < 6; j++) {
 				System.out.print(board[i][j] + " ");
