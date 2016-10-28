149f3bae2e0a304814584982181c744f8e65efef
ns.bisht <ns.bisht@BW-NS-BISH-700.sea.samsung.com>
2016-10-28 16:02:39 -0400

    Added the string reverse method

--- CodingProblems/src/com/narren/coding/practice/ReverseString.java
+++ CodingProblems/src/com/narren/coding/practice/ReverseString.java
@@ -2,5 +2,18 @@ package com.narren.coding.practice;
 
 public class ReverseString {
 
-	static String reverseString
+	public static void main(String[] args) {
+		System.out.println(reverseString("aaaa"));
+		
+	}
+	
+	static String reverseString(String input) {
+		char[] in = input.toCharArray();
+		for(int i = 0, j = in.length  - 1; i < j; i++, j--) {
+			char temp = in[i];
+			in[i] = in[j];
+			in[j] = temp;
+		}
+		return new String(in);
+	}
 }
