e1cd4fc2bbac5ca5a30bd69dcfd44cd3f34337e8
NarenHome <narren.singh@gmail.com>
2017-01-18 21:06:23 -0500

    Added one more approach to solve the problem

--- CodingProblems/src/com/narren/leetCode/KthSmallestElementinSortedMatrix.java
+++ CodingProblems/src/com/narren/leetCode/KthSmallestElementinSortedMatrix.java
@@ -1,5 +1,8 @@
 package com.narren.leetCode;
 
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
 /**
  * 
 Given a n x n matrix where each of the rows and columns are sorted in ascending order, find the kth smallest element in the matrix.
@@ -26,33 +29,83 @@ https://leetcode.com/problems/kth-smallest-element-in-a-sorted-matrix/
  */
 public class KthSmallestElementinSortedMatrix {
 
+	/**
+	 * Version 1 : using binary search
+	 * @param matrix
+	 * @param k
+	 * @return
+	 */
+//	public int kthSmallest(int[][] matrix, int k) {
+//        int lower = matrix[0][0];
+//        int upper = matrix[matrix.length - 1][matrix.length - 1];
+//        while(lower < upper) {
+//            int mid = lower + ((upper - lower) >> 1);
+//            int count = countOfSmallNum(matrix, mid);
+//            if(count < k) {
+//            	lower = mid + 1;
+//            } else {
+//            	upper = mid;
+//            }
+//        }
+//        return upper;
+//    }
+//
+//    int countOfSmallNum(int[][] matrix, int mid) {
+//        int count = 0;
+//        int i = matrix.length - 1;
+//        int j = 0;
+//        while(j < matrix.length && i >= 0) {
+//            if(matrix[i][j] <= mid) {
+//                count += i + 1;
+//                j++;
+//            } else {
+//                i--;
+//            }
+//        }
+//        return count;
+//    }
+	
+	/**
+	 * Version 2 : Using min-heap
+	 * @param matrix
+	 * @param k
+	 * @return
+	 */
 	public int kthSmallest(int[][] matrix, int k) {
-        int lower = matrix[0][0];
-        int upper = matrix[matrix.length - 1][matrix.length - 1];
-        while(lower < upper) {
-            int mid = lower + ((upper - lower) >> 1);
-            int count = countOfSmallNum(matrix, mid);
-            if(count < k) {
-            	lower = mid + 1;
-            } else {
-            	upper = mid;
-            }
-        }
-        return upper;
-    }
-
-    int countOfSmallNum(int[][] matrix, int mid) {
-        int count = 0;
-        int i = matrix.length - 1;
-        int j = 0;
-        while(j < matrix.length && i >= 0) {
-            if(matrix[i][j] <= mid) {
-                count += i + 1;
-                j++;
-            } else {
-                i--;
-            }
-        }
-        return count;
-    }
+		PriorityQueue<Element> queue = new PriorityQueue<Element>(k + 1);
+		for(int i = 0 ; i < matrix.length; i++) {
+			queue.offer(new Element(0, i, matrix[0][i]));
+		}
+		
+		int i = 1;
+		while(i <= k) {
+			Element root = queue.poll();
+			if(i == k) {
+				return root.val;
+			}
+			if(root.r + 1 < matrix.length) {
+				queue.offer(new Element(root.r + 1, root.c, matrix[root.r + 1][root.c]));				
+			}
+			i++;
+		}
+		return -1;
+		
+	}
+}
+class Element implements Comparable<Element> {
+	int r;
+	int c;
+	int val;
+	
+	public Element(int x, int y, int v) {
+		r = x;
+		c = y;
+		val = v;
+	}
+	
+	@Override
+	public int compareTo(Element o) {
+		return this.val - o.val;
+	}
+	
 }
