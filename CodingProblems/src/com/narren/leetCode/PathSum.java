a07a2446f67e325c6e5d94f58f11210a4790156f
Naren <ns.bisht@samsung.com>
2017-02-15 15:01:53 -0500

    Added first version path sum III

--- CodingProblems/src/com/narren/leetCode/PathSum.java
+++ CodingProblems/src/com/narren/leetCode/PathSum.java
@@ -51,10 +51,3 @@ public class PathSum {
 
 	}
 }
-
-class TreeNode {
-	int val;
-	TreeNode left;
-	TreeNode right;
-	TreeNode(int x) { val = x; }
-}
---
+++ CodingProblems/src/com/narren/leetCode/PathSumIII.java
@@ -0,0 +1,60 @@
+package com.narren.leetCode;
+
+import java.util.HashMap;
+
+/**
+ * 
+You are given a binary tree in which each node contains an integer value.
+
+Find the number of paths that sum to a given value.
+
+The path does not need to start or end at the root or a leaf, but it must
+go downwards (traveling only from parent nodes to child nodes).
+
+The tree has no more than 1,000 nodes and the values are in the range -1,000,000 to 1,000,000.
+
+Example:
+
+root = [10,5,-3,3,2,null,11,3,-2,null,1], sum = 8
+
+      10
+     /  \
+    5   -3
+   / \    \
+  3   2   11
+ / \   \
+3  -2   1
+
+Return 3. The paths that sum to 8 are:
+
+1.  5 -> 3
+2.  5 -> 2 -> 1
+3. -3 -> 11
+ * 
+ * 
+https://leetcode.com/problems/path-sum-iii/
+ * 
+ * @author nsbisht
+ *
+ */
+public class PathSumIII {
+
+	public int pathSum(TreeNode root, int sum) {
+		return calculatePathSum(root, sum, 0, new HashMap<Integer, Integer>());
+	}
+
+	int calculatePathSum(TreeNode root, int desired, int curSum, HashMap<Integer, Integer> map) {
+		if(root == null) {
+			return 0;
+		}
+		int newSum = root.val + curSum;
+		int res = map.getOrDefault(newSum - desired, 0);
+		map.put(newSum, map.getOrDefault(newSum, 0) + 1);
+		int left = calculatePathSum(root.left, desired, newSum, map);
+		int right = calculatePathSum(root.right, desired, newSum, map);
+		res += left + right;
+		map.remove(newSum);
+		return res;
+	}
+}
+
