6c75735f3239fc911a2635caf1d9bfb9d70d1c9c
NarenHome <narren.singh@gmail.com>
2017-02-23 07:43:54 -0500

    LCA problem

---
+++ CodingProblems/src/com/narren/sotong/Bidding.java
@@ -0,0 +1,16 @@
+package com.narren.sotong;
+
+public class Bidding {
+
+	int bidding(int[][] bids, int itemCount, int bidderCount) {
+		int[] itemArray = new int[itemCount];
+		int[] bidderArray = new int[bidderCount];
+		
+		for(int i = 0; i < bidderCount; i++) {
+			for(int j = 0; j < bidderCount; j++) {
+				
+			}
+		}
+		
+	}
+}
