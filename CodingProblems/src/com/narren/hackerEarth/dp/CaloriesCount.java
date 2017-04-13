cd00e70e992b9cdf110236ceb2febfbff6d7fe19
NarenHome <narren.singh@gmail.com>
2017-04-13 08:15:04 -0400

    updated the comments

--- CodingProblems/src/com/narren/hackerEarth/dp/CaloriesCount.java
+++ CodingProblems/src/com/narren/hackerEarth/dp/CaloriesCount.java
@@ -1,14 +1,45 @@
 package com.narren.hackerEarth.dp;
 /**
- * Problem Statement :
-You are given a 2-D matrix A of n rows and m columns where A[i][j] denotes the calories burnt. Two persons, a boy and a girl,
-start from two corners of this matrix. The boy starts from cell (1,1) and needs to reach cell (n,m). On the other hand, the girl starts
-from cell (n,1) and needs to reach (1,m). The boy can move right and down. The girl can move right and up. As they visit a cell, the amount in
-the cell A[i][j] is added to their total of calories burnt. You have to maximize the sum of total calories burnt by both of them under the condition
-that they shall meet only in one cell and the cost of this cell shall not be included in either of their total.
+ * 
+ * Working out *
+Summer is coming! It's time for Iahub and Iahubina to work out, as they both want to look hot at the beach.
+The gym where they go is a matrix a with n lines and m columns. Let number a[i][j] represents the calories burned by performing workout at
+the cell of gym in the i-th line and the j-th column.
+
+Iahub starts with workout located at line 1 and column 1. He needs to finish with workout a[n][m]. After finishing workout a[i][j],
+he can go to workout a[i + 1][j] or a[i][j + 1]. Similarly, Iahubina starts with workout a[n][1] and she needs to finish with workout a[1][m].
+After finishing workout from cell a[i][j], she goes to either a[i][j + 1] or a[i - 1][j].
+
+There is one additional condition for their training. They have to meet in exactly one cell of gym. At that cell, none of them will work out.
+They will talk about fast exponentiation (pretty odd small talk) and then both of them will move to the next workout.
+
+If a workout was done by either Iahub or Iahubina, it counts as total gain. Please plan a workout for Iahub and Iahubina such as total gain
+to be as big as possible. Note, that Iahub and Iahubina can perform workouts with different speed, so the number of cells that they use to
+reach meet cell may differs.
+
+Input
+The first line of the input contains two integers n and m (3 ≤ n, m ≤ 1000). Each of the next n lines contains m integers: j-th number
+from i-th line denotes element a[i][j] (0 ≤ a[i][j] ≤ 105).
+
+Output
+The output contains a single number — the maximum total gain possible.
+
+Examples
+input
+3 3
+100 100 100
+100 1 100
+100 100 100
+output
+800
+Note
+Iahub will choose exercises a[1][1] → a[1][2] → a[2][2] → a[3][2] → a[3][3]. Iahubina will choose exercises a[3][1] → a[2][1] → a[2][2] → a[2][3] → a[1][3].
 
 
 https://www.hackerearth.com/practice/algorithms/dynamic-programming/2-dimensional/tutorial/
+
+http://codeforces.com/problemset/problem/429/B
+
  * @author naren
  *
  */
