99056cbadb9a1e271a23ae07b41e47d2d07b650a
NarenHome <narren.singh@gmail.com>
2017-04-26 22:59:14 -0400

    longest line of one in Matrix

--- CodingProblems/src/com/narren/leetCode/LongestLineofConsecutiveOneinMatrix.java
+++ CodingProblems/src/com/narren/leetCode/LongestLineofConsecutiveOneinMatrix.java
@@ -18,4 +18,84 @@ https://leetcode.com/contest/leetcode-weekly-contest-29/problems/longest-line-of
  */
 public class LongestLineofConsecutiveOneinMatrix {
 
+	public static void main(String[] args) {
+		int[][] cost = new int[][]{
+			{0, 1, 1, 0},
+			{0, 0, 1, 0},
+			{0, 1, 0, 0},
+			{1, 0, 0, 0}}; 
+			
+			System.out.println(new LongestLineofConsecutiveOneinMatrix().longestLine(cost));
+	}
+	public int longestLine(int[][] M) {
+
+		int max = 0;
+		for(int i = 0; i < M.length; i++) {
+			for(int j = 0; j < M[0].length; j++) {
+				if(M[i][j] == 1) {
+					max = Math.max(max, getMax(M, i, j));
+				}
+			}
+		}
+		
+		return max;
+	}
+	
+	int getMax(int[][] M, int i, int j) {
+		int max = 0;
+		if(j == 0 || M[i][j - 1] == 0) {
+			max = rightMax(M, i, j + 1, 1);
+		}
+		if(i == 0 || M[i - 1][j] == 0) {
+			max = Math.max(max, downMax(M, i + 1, j, 1));
+		}
+		if(j == 0 || i == 0 || M[i - 1][j - 1] == 0) {
+			max = Math.max(max, diagRMax(M, i + 1, j + 1, 1));
+		}
+		if(j == M[0].length - 1 || i == 0 || M[i - 1][j + 1] == 0) {
+			max = Math.max(max, diagLMax(M, i + 1, j - 1, 1));
+		}
+		
+		return max;
+	}
+	
+	int rightMax(int[][] M, int i, int j, int count) {
+		if(j >= M[0].length) {
+			return count;
+		}
+		if(M[i][j] == 0) {
+			return count;
+		}
+		return rightMax(M, i, j + 1, count + 1);
+	}
+	
+	int downMax(int[][] M, int i, int j, int count) {
+		if(i >= M.length) {
+			return count;
+		}
+		if(M[i][j] == 0) {
+			return count;
+		}
+		return downMax(M, i + 1, j, count + 1);
+	}
+	
+	int diagRMax(int[][] M, int i, int j, int count) {
+		if(i >= M.length || j >= M[0].length) {
+			return count;
+		}
+		if(M[i][j] == 0) {
+			return count;
+		}
+		return diagRMax(M, i + 1, j + 1, count + 1);
+	}
+	
+	int diagLMax(int[][] M, int i, int j, int count) {
+		if(i >= M.length || j < 0) {
+			return count;
+		}
+		if(M[i][j] == 0) {
+			return count;
+		}
+		return diagLMax(M, i + 1, j - 1, count + 1);
+	}
 }
