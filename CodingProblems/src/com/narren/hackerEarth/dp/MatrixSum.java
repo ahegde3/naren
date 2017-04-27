35c62145f725377cf477ef4043bfc832914f39a2
Naren <ns.bisht@samsung.com>
2017-04-27 15:51:05 -0400

    updated Matrix sum

--- CodingProblems/src/com/narren/hackerEarth/dp/MatrixSum.java
+++ CodingProblems/src/com/narren/hackerEarth/dp/MatrixSum.java
@@ -4,48 +4,26 @@ import java.util.Scanner;
 
 public class MatrixSum {
 
-	static int[][] dp;
-
-	public static void main(String[] args) {
+	public static void main(String[] args) {{
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
 		int M = sc.nextInt();
-		dp = new int[N][M];
-		int[][] in = new int[N][M];
+		long[][]dp = new long[N][M];
+		long[][] in = new long[N][M];
+		long curSum = 0;
 		for(int i = 0; i < N; i++) {
 			for(int j = 0; j < M; j++) {
 				in[i][j] = sc.nextInt();
+				curSum += in[i][j];
+				dp[i][j] = curSum;
 			}
 		}
-		dp[0][0] = in[0][0];
 		int cases = sc.nextInt();
 		while(cases > 0) {
 			int x = sc.nextInt();
 			int y = sc.nextInt();
-			System.out.println(findSum(in, x -1 , y - 1));
+			System.out.println(dp[x - 1][y - 1]);
 			cases--;
 		}
-	}
-	static int findSum(int[][] matrix, int x, int y) {
-		if(x < 0 || y < 0 || x >= matrix.length || y >= matrix[0].length) {
-			return 0;
-		}
-
-		if(dp[x][y] > 0) {
-			return dp[x][y];
-		}
-		int px = x, py = y;
-		if(x == 0) {
-			py--;
-		} else if(y == 0) {
-			px--;
-			py = matrix[0].length - 1;
-		} else {
-			py--;
-		}
-
-		int n = findSum(matrix, px, py);
-		dp[x][y] = n + matrix[x][y];
-		return dp[x][y];
-	}
+	}}
 }
