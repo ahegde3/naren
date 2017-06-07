819b7fec7585a4bf8aca20df4b0a98185d7af90c
NarenHome <narren.singh@gmail.com>
2017-06-06 23:35:22 -0400

    MergeSort

--- CodingProblems/src/com/narren/hackerEarth/sorting/MergeSort.java
+++ CodingProblems/src/com/narren/hackerEarth/sorting/MergeSort.java
@@ -2,25 +2,62 @@ package com.narren.hackerEarth.sorting;
 
 public class MergeSort {
 
+	public static void main(String[] args) {
+		MergeSort ms = new MergeSort();
+		int[] arr = new int[]{5, 4, 3, 10, 9, 2};
+		ms.sort(arr);
+		for(int i : arr) {
+			System.out.print(i + " ");
+		}
+	}
 	public void sort(int[] arr) {
-
+		mergeSort(arr, 0, arr.length - 1);
 	}
 
 	void mergeSort(int[] arr, int s, int e) {
-		if(s > e) {
+		if(s < e) {
 			int m = (s + e) >>> 1;
 
 			mergeSort(arr, s, m);
 
 			mergeSort(arr, m + 1, e);
 			
-			merge(arr, s, e, m);
+			merge(arr, s, m, e);
 		}
 	}
 
-	void merge(int[] arr, int s, int e, int m) {
-		for(int i = s; i <= m ; i++) {
-			
-		}
-	}
+	 void merge(int arr[ ] , int start, int mid, int end) {
+		 int[] temp = new int[(end - start) + 1];
+		 int s1 = start;
+		 int s2 = mid + 1;
+		 int index = 0;
+		 
+		 for(int i = start; i <= end; i++) {
+			 if(s1 > mid) {
+				 temp[index++] = arr[s2];
+				 s2++;
+				 continue;
+			 }
+			 if(s2 > end) {
+				 temp[index++] = arr[s1];
+				 s1++;
+				 continue;
+				 
+			 }
+			 if(arr[s1] > arr[s2]) {
+				 temp[index++] = arr[s2];
+				 s2++;
+				 System.out.println("*");
+			 } else {
+				 temp[index++] = arr[s1];
+				 s1++;
+			 }
+			 
+		 }
+		 index = 0;
+		 for(int i = start; i <= end; i++) {
+			 arr[i] = temp[index++];
+		 }
+		 
+	 }
 }
---
+++ CodingProblems/src/com/narren/hackerEarth/sorting/MergeSortProb.java
@@ -0,0 +1,11 @@
+package com.narren.hackerEarth.sorting;
+/**
+ Given an array  A  on size  N N you need to find the number of ordered pairs  (i,j) such that  i < j and  A[i]>A[j].
+ 
+ https://www.hackerearth.com/practice/algorithms/sorting/merge-sort/practice-problems/algorithm/mergesort/
+ * @author naren
+ *
+ */
+public class MergeSortProb {
+
+}
