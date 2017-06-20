fcca50dc251e811b75a82122ad35b0f70e26364e
NarenHome <narren.singh@gmail.com>
2017-06-19 23:21:36 -0400

    Solved Greedy algo ques

--- CodingProblems/src/com/narren/hackerEarth/greedy/BeingGreedyforWater.java
+++ CodingProblems/src/com/narren/hackerEarth/greedy/BeingGreedyforWater.java
@@ -19,53 +19,31 @@ SAMPLE OUTPUT
  */
 public class BeingGreedyforWater {
 
-	static long maxBotttle = 0; 
-
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
 
 		while(T > 0) {
 			int N = sc.nextInt();
-			int X = sc.nextInt();
+			long X = sc.nextInt();
 			long[] bottles = new long[N];
 
 			for(int i = 0; i < N; i++) {
 				bottles[i] = sc.nextInt();
 			}
 			Arrays.sort(bottles);
-			for(int i = 0; i < N; i++) {
-				if(bottles[i] > X) {
-					break;
-				}
-				fillWater(bottles, i, 0, X, 1);
-			}
-			System.out.println(maxBotttle);
-			maxBotttle = 0;
 
+			int count = 0;
+			for(int j = 0;j < N;j++){
+				if(bottles[j] <= X){
+					X -= bottles[j];
+					count++;
+				}
+				else
+					break;
+			}
+			System.out.println(count);
 			T--;
 		}
 	}
-
-    static int fillWater(long[] b, int index, long sum, int X, int bottles) {
-    	System.out.println(index);
-        if(index >= b.length) {
-            return -1;
-        }
-        if(b[index] + sum == X) {
-            maxBotttle = Math.max(maxBotttle, bottles);
-            return 0;
-        }
-
-        if(b[index] + sum > X) {
-            return -2;
-        }
-        for(int i = index + 1; i < b.length; i++) {
-        	int c = fillWater(b, i, sum + b[index], X, bottles + 1);
-            if(c == -2 || c == -1 || c == 0) {
-                return 1;
-            }
-        }
-        return 1;
-    }
 }
