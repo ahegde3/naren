cc110ef6e404cfd8f96a3cc58dbb0da6e2e185d9
NarenHome <narren.singh@gmail.com>
2017-06-24 15:52:36 -0400

    Updated the buying two times prob

--- CodingProblems/src/com/narren/leetCode/BestTimetoBuyandSellStockIII.java
+++ CodingProblems/src/com/narren/leetCode/BestTimetoBuyandSellStockIII.java
@@ -16,33 +16,35 @@ https://leetcode.com/problems/best-time-to-buy-and-sell-stock-iii/
 public class BestTimetoBuyandSellStockIII {
 
 	public static void main(String[] args) {
-		System.out.println(new BestTimetoBuyandSellStockIII().maxProfit(new int[]{12, 5}));
+		System.out.println(new BestTimetoBuyandSellStockIII().maxProfit(new int[]{1,2,4,2,5,7,2,4,9,0}));
 	}
-	 public int maxProfit(int[] prices) {
-	        return getProfit(prices, 2);
-	    }
+	public int maxProfit(int[] prices) {
+		if(prices.length < 2) {
+			return 0;
+		}
+		return getProfit(prices, 2);
+	}
 
-	    int getProfit(int[] prices, int k) {
-	        /**
-	         * Formula
-	         * p[t][i] = max {p[t][i - 1] (no transaction),
-	         *
-	         *                max(p[i] - p[j] + p[t - 1][j]), where 0 <= j < i
-	         *
-	         *               }
-	         */
-	        int[][] profit = new int[k + 1][prices.length + 1];
+	int getProfit(int[] prices, int k) {
+		/**
+		 * Formula
+		 * p[t][i] = max {p[t][i - 1] (no transaction),
+		 *
+		 *                maxDiff + prices[j - 1], where maxDiff = max(maxDiff, profit[i - 1][j - 1] - prices[j - 2])
+		 *
+		 *               }
+		 */
+		int[][] profit = new int[k + 1][prices.length + 1];
 
-	        for(int i = 1; i <= k; i++) {
-	            for(int j = 1; j < prices.length + 1; j++) {
-	                int max1 = 0;
-	                for(int l = 1; l < j; l++) {
-	                    max1 = Math.max(max1, prices[j - 1] - prices[l - 1] + profit[i - 1][l]);
-	                }
-	                profit[i][j] = Math.max(max1, profit[i][j - 1]);
-	            }
-	        }
-	        return profit[k][prices.length];
-	    }
-	
+		for(int i = 1; i <= k; i++) {
+			int maxDiff = -prices[0]; 
+			for(int j = 2; j < prices.length + 1; j++) {
+				maxDiff = Math.max(maxDiff, profit[i - 1][j - 1] - prices[j - 2]);
+				int max1 = 0;
+				profit[i][j] = Math.max(profit[i][j - 1], maxDiff + prices[j - 1]);
+			}
+		}
+		return profit[k][prices.length];
+	}
+
 }
