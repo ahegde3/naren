7f39847ab8e1bd1f6fd4005ea7f281d494eae996
NarenHome <narren.singh@gmail.com>
2017-06-26 23:31:35 -0400

    weekly contest 38

--- CodingProblems/src/com/narren/leetCode/MaximumProductofThreeNumbers.java
+++ CodingProblems/src/com/narren/leetCode/MaximumProductofThreeNumbers.java
@@ -18,13 +18,29 @@ Multiplication of any three numbers in the input won't exceed the range of 32-bi
  *
  */
 public class MaximumProductofThreeNumbers {
+	
 	public int maximumProduct(int[] nums) {
-		Arrays.sort(nums);
-		int res = 1;
-		for(int i = nums.length - 1; i >= 0; i--) {
-			res *= nums[i];
+			
+		if(nums.length == 3) {
+			return nums[0] * nums[1] * nums[2];
 		}
-
-		return res;
+		
+		if(nums.length < 6) {
+			int tres = Integer.MIN_VALUE;
+			for(int i = 0; i < nums.length; i++) {
+				for(int j = i + 1; j < nums.length; j++) {
+					for(int k = j + 1; k < nums.length; k++) {
+						tres = Math.max(tres, nums[i] * nums[j] * nums[k]);
+					}
+				}
+			}
+			return tres;
+		} else {
+			Arrays.sort(nums);
+			
+			int n1 = nums[0] * nums[1] * nums[nums.length - 1];
+			int n2 = nums[nums.length - 3] * nums[nums.length - 2] * nums[nums.length - 1];
+			return Math.max(n1, n2);
+		}
 	}
 }
