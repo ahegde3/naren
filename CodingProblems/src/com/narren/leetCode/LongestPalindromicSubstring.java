18779b015ebf5c84c8b2013fbdc5c3de70501b2d
Naren <ns.bisht@samsung.com>
2017-07-11 18:12:09 -0400

    second cut longest palindromw

--- CodingProblems/src/com/narren/leetCode/LongestPalindromicSubstring.java
+++ CodingProblems/src/com/narren/leetCode/LongestPalindromicSubstring.java
@@ -20,6 +20,37 @@ Output: "bb"
  */
 public class LongestPalindromicSubstring {
 	public String longestPalindrome(String s) {
+		int[] index = new int[2];
+		char[] chars = s.toCharArray();
+		for(int i = 0; i < chars.length; i++) {
+			for(int j = chars.length - 1; j > i; j--) {
+				if(isPalindrome(chars, i, j)) {
+					if(j - i > index[1] - index[0]) {
+						index[0] = i;
+						index[1] = j;
+						break;
+					}
+				}
 
+			}
+		}
+
+		String ret = "";
+		for(int i = index[0]; i <= index[1]; i++) {
+			ret += s.substring(i);
+		}
+		return ret;
+	}
+
+
+	boolean isPalindrome(char[] c, int s, int e) {
+		while(s < e) {
+			if(c[s] != c[e]) {
+				return false;
+			}
+			s++;
+			e--;
+		}
+		return true;
 	}
 }
