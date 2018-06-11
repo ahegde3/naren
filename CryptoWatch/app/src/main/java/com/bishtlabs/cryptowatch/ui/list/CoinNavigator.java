71600d02e57c0e216878fdf250aae8ddd1d67711
Naren <ns.bisht@samsung.com>
2018-06-11 17:09:04 -0400

    Added the navigator interface

---
+++ CryptoWatch/app/src/main/java/com/bishtlabs/cryptowatch/ui/list/CoinNavigator.java
@@ -0,0 +1,9 @@
+package com.bishtlabs.cryptowatch.ui.list;
+
+/**
+ * Created by nsbisht on 6/11/18.
+ */
+
+public interface CoinNavigator {
+    void openCoinDetail(int position);
+}
--- CryptoWatch/app/src/main/java/com/bishtlabs/cryptowatch/ui/list/CoinsListViewModel.java
+++ CryptoWatch/app/src/main/java/com/bishtlabs/cryptowatch/ui/list/CoinsListViewModel.java
@@ -1,8 +1,18 @@
 package com.bishtlabs.cryptowatch.ui.list;
 
+import com.bishtlabs.cryptowatch.data.DataManager;
+import com.bishtlabs.cryptowatch.ui.base.BaseViewHolder;
+import com.bishtlabs.cryptowatch.ui.base.BaseViewModel;
+
 /**
  * Created by naren on 6/1/18.
  */
 
-public class CoinsListViewModel {
+public class CoinsListViewModel extends BaseViewModel<CoinNavigator> {
+
+    public CoinsListViewModel(DataManager mDataManager) {
+        super(mDataManager);
+    }
+
+
 }
